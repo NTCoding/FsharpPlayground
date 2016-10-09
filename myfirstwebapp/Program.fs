acf9966273259d52b0aa0a77a36f2d03eb9f75e5
Nick Tune <ntune@ntune-ltm.internal.salesforce.com>
2016-10-09 17:35:30 +0100

    wire up asp.net mvc

--- myfirstwebapp/Program.fs
+++ myfirstwebapp/Program.fs
@@ -1,17 +1,37 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open System.IO
 open Microsoft.AspNetCore.Hosting
 open Microsoft.AspNetCore.Http
 open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.AspNetCore.Http
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.Configuration
+open Microsoft.Extensions.Logging
 
-type Startup() = 
-    member this.Configure(app: IApplicationBuilder) =
-      app.Run(fun context -> context.Response.WriteAsync("Server running"))
+type Startup() =
+
+    let configuration = 
+        ConfigurationBuilder().AddEnvironmentVariables().Build()
+
+    // This method gets called by the runtime.
+    // Use this method to add dependencies to the container.
+    member __.ConfigureServices(container : IServiceCollection) =
+        container.AddMvc() |> ignore
+
+    // This method gets called by the runtime.
+    // Use this method to configure the HTTP request pipeline.
+    member __.Configure (app : IApplicationBuilder)
+                        (env : IHostingEnvironment)
+                        (loggerFactory : ILoggerFactory) =
+        app.UseMvc() |> ignore
 
 [<EntryPoint>]
 let main argv = 
-    let host = WebHostBuilder().UseKestrel().UseStartup<Startup>().Build()
+    let host = WebHostBuilder().UseKestrel().UseContentRoot(Directory.GetCurrentDirectory()).UseStartup<Startup>().Build()
     host.Run()
     0
     
