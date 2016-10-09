c4567df3de63b1f1fd1a2ae58b63881ae80d76d0
Nick Tune <ntune@ntune-ltm.internal.salesforce.com>
2016-10-09 17:04:30 +0100

    start up kestrel web server

--- myfirstwebapp/Program.fs
+++ myfirstwebapp/Program.fs
@@ -1,8 +1,17 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open Microsoft.AspNetCore.Hosting
+open Microsoft.AspNetCore.Http
+open Microsoft.AspNetCore.Builder
+
+type Startup() = 
+    member this.Configure(app: IApplicationBuilder) =
+      app.Run(fun context -> context.Response.WriteAsync("Server running"))
 
 [<EntryPoint>]
 let main argv = 
-    printfn "Hello World!"
-    0 // return an integer exit code
+    let host = WebHostBuilder().UseKestrel().UseStartup<Startup>().Build()
+    host.Run()
+    0
+    
